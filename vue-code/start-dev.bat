814173e531867dd3387ec7e12ecae4a57ab37add
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:55:22 +0800

    XianYuZP 0.0.1

---
+++ vue-code/start-dev.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ========================================
+echo XianYuZP - Vue3 前端
+echo ========================================
+echo.
+echo 正在启动开发服务器...
+echo.
+cd /d %~dp0
+npm run dev
