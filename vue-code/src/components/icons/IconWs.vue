5e56f9fde48e1617f7bafeb80aa9ba3f11b4fddb
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:48:15 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconWs.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+    <text x="12" y="17" text-anchor="middle" font-size="10" font-weight="700" font-family="system-ui, sans-serif">WS</text>
+  </svg>
+</template>
