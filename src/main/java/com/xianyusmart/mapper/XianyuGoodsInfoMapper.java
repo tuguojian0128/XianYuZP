6f195893ea6a86ae6f207ee2d267e67b94f8dc35
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:30 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/XianyuGoodsInfoMapper.java
@@ -0,0 +1,12 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.XianyuGoodsInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 闲鱼商品信息Mapper
+ */
+@Mapper
+public interface XianyuGoodsInfoMapper extends BaseMapper<XianyuGoodsInfo> {
+}
