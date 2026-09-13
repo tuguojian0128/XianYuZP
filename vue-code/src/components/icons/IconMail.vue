c231ff93917540d6aed195225a6212214b5d24ce
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:46:05 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconMail.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <rect x="2" y="4" width="20" height="16" rx="3" />
+    <path d="M22 7l-10 6L2 7" />
+  </svg>
+</template>
