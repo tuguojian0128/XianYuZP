f4a4268a65a52c1a5ea76e9b8f627f3f66054957
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:22:56 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/config/CustomLocalDateTimeTypeHandler.java
@@ -0,0 +1,34 @@
+package com.xianyusmart.config;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class CustomLocalDateTimeTypeHandler extends BaseTypeHandler<LocalDateTime> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, LocalDateTime parameter, JdbcType jdbcType) throws SQLException {
+        // 使用JDBC时间类型保留DATETIME(3)的毫秒精度
+        ps.setTimestamp(i, Timestamp.valueOf(parameter));
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        Timestamp ts = rs.getTimestamp(columnName);
+        return ts == null ? null : ts.toLocalDateTime();
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        Timestamp ts = rs.getTimestamp(columnIndex);
+        return ts == null ? null : ts.toLocalDateTime();
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        Timestamp ts = cs.getTimestamp(columnIndex);
+        return ts == null ? null : ts.toLocalDateTime();
+    }
+}
