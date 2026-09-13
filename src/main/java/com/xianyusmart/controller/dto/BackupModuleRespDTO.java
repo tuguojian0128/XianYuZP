b6349d629a04513b7ae8339f90264fc356075a56
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:26:30 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/BackupModuleRespDTO.java
@@ -0,0 +1,11 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BackupModuleRespDTO {
+    private String moduleKey;
+    private String moduleName;
+}
