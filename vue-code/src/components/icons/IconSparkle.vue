2cb9ad333e55b9275b9151424575c752373a88bb
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:46:55 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconSparkle.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M12 3l1.5 4.5L18 9l-4.5 1.5L12 15l-1.5-4.5L6 9l4.5-1.5L12 3z" />
+    <path d="M18 14l.75 2.25L21 17l-2.25.75L18 20l-.75-2.25L15 17l2.25-.75L18 14z" />
+  </svg>
+</template>
