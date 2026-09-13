5f522f11b8e9d7adc6aa7f4cedd6fd0e897a7af5
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:28:54 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/RefreshItemsRespDTO.java
@@ -0,0 +1,15 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RefreshItemsRespDTO {
+    private Boolean success;
+    private Integer totalCount;
+    private Integer successCount;
+    private List<String> updatedItemIds;
+    private String message;
+    private String syncId;
+}
