8c4abf659e474c5ed6d9eb9146304c1e8a5275ee
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:48:18 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/layout/LoginLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="login-layout">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.login-layout {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999;
+  background: #e8e8e8;
+}
+</style>
