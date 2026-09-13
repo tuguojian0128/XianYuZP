a7094de96d0692069c70c4e132c4392ab5a04cc1
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:27:56 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/LoginReqDTO.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * 登录请求DTO
+ * @date 2026/4/22
+ */
+@Data
+public class LoginReqDTO {
+    private String username;
+    private String password;
+}
