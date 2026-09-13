f3117687ee6884fbf541df4b5ce7fa79d454c5a3
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:35 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/XianyuGoodsSkuPropertyMapper.java
@@ -0,0 +1,9 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.XianyuGoodsSkuProperty;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface XianyuGoodsSkuPropertyMapper extends BaseMapper<XianyuGoodsSkuProperty> {
+}
