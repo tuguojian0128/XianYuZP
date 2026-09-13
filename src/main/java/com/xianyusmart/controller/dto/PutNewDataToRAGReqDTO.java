7739d5b94d7eaed758e16ffae6e3dedc6e2457c4
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:28:45 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/PutNewDataToRAGReqDTO.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * @date 2026/4/12 00:42
+ * @description
+ */
+@Data
+public class PutNewDataToRAGReqDTO {
+    String content;
+    String goodsId;
+}
