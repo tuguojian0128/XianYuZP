41526cd98d19edb91fbacf5aa8b4e47e34310d5a
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:45:36 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconHelp.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="12" cy="12" r="10" />
+    <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+    <circle cx="12" cy="17" r="0.5" fill="currentColor" />
+  </svg>
+</template>
