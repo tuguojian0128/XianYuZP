9946852a13082fb19800fe3b31a23559ad0ea1ff
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:10 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/SysLoginTokenMapper.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.SysLoginToken;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 登录Token Mapper
+ * @date 2026/4/22
+ */
+@Mapper
+public interface SysLoginTokenMapper extends BaseMapper<SysLoginToken> {
+}
