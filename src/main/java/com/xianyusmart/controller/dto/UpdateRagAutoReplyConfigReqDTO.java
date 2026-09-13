653ffe55b49b67e411dc25ff5ff7eb9594067bd9
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:29:24 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/UpdateRagAutoReplyConfigReqDTO.java
@@ -0,0 +1,15 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * 更新自动回复配置请求DTO
+ * @date 2026/4/22
+ */
+@Data
+public class UpdateRagAutoReplyConfigReqDTO {
+    private Long xianyuAccountId;  
+    private String xyGoodsId;
+    /** 回复延时秒数 */
+    private Integer ragDelaySeconds;
+}
