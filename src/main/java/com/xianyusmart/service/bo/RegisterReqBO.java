07110c03d2118eb13b3070a2e1b767cec20cdf1f
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:34:42 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/service/bo/RegisterReqBO.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.service.bo;
+
+import lombok.Data;
+
+/**
+ * 注册请求BO
+ * @date 2026/4/22
+ */
+@Data
+public class RegisterReqBO {
+    private String username;
+    private String password;
+}
