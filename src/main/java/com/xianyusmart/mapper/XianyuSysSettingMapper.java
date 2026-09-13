83d0cbfc39746c80c54b884861577b94bceaeb7b
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:56 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/XianyuSysSettingMapper.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.XianyuSysSetting;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 系统配置Mapper
+ * @date 2026/4/22
+ */
+@Mapper
+public interface XianyuSysSettingMapper extends BaseMapper<XianyuSysSetting> {
+}
