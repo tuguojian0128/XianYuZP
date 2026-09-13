ec3549626dec98ccf39aa544164051137d614c10
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:26:52 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/CheckUserExistsRespDTO.java
@@ -0,0 +1,12 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * 检查用户是否存在响应DTO
+ * @date 2026/4/22
+ */
+@Data
+public class CheckUserExistsRespDTO {
+    private Boolean exists;
+}
