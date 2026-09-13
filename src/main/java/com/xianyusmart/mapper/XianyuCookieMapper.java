833d941a9fa5777185e8c62bffd81882bc5bfe62
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:20 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/XianyuCookieMapper.java
@@ -0,0 +1,12 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.XianyuCookie;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 闲鱼Cookie Mapper
+ */
+@Mapper
+public interface XianyuCookieMapper extends BaseMapper<XianyuCookie> {
+}
