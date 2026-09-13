43f86ca18f121e8303e9771ed2cb46e8a27a1c2b
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:45:45 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconKey.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.78 7.78 5.5 5.5 0 0 1 7.78-7.78zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4" />
+  </svg>
+</template>
