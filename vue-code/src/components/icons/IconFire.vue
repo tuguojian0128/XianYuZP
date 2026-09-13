2ee8ae7a1fcf226c23d474080013f5ec027006da
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:45:34 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconFire.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M12 22c-4 0-7-3-7-7 0-2 1-4 2-5.5C7.5 8 8 6 8 4c2 2 3.5 3 4 5 .5-2 2-3 4-5 0 2 .5 4 1 5.5C18 11 19 13 19 15c0 4-3 7-7 7z" />
+    <path d="M12 22c-2 0-3.5-1.5-3.5-3.5 0-1 .5-2 1.5-3 .5 1 1.5 1.5 2 1.5s1.5-.5 2-1.5c1 1 1.5 2 1.5 3 0 2-1.5 3.5-3.5 3.5z" />
+  </svg>
+</template>
