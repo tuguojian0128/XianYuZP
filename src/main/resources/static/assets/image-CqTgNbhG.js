5bfa98653bddc6b17bbea8bbbb3eac61dc3293c1
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:38:45 +0800

    XianYuZP 0.0.1

---
+++ src/main/resources/static/assets/image-CqTgNbhG.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-BmZXdVT7.js";function o(e,r){const a=new FormData;return a.append("accountId",String(e)),a.append("file",r),t({url:"/image/upload",method:"POST",data:a,headers:{"Content-Type":"multipart/form-data"}})}function s(e){return t({url:"/websocket/sendImageMessage",method:"POST",data:e})}export{s,o as u};
