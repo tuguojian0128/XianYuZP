ebfe44b2f50c1bf2cc06510c7ad6b230371cd492
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:28:58 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/SaveSettingReqDTO.java
@@ -0,0 +1,14 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * 保存配置请求DTO
+ * @date 2026/4/22
+ */
+@Data
+public class SaveSettingReqDTO {
+    private String settingKey;
+    private String settingValue;
+    private String settingDesc;
+}
