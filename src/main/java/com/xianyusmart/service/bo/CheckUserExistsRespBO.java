dd38867eed746f63b681f335979dc2d621dcf0ae
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:34:25 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/service/bo/CheckUserExistsRespBO.java
@@ -0,0 +1,12 @@
+package com.xianyusmart.service.bo;
+
+import lombok.Data;
+
+/**
+ * 检查用户是否存在响应BO
+ * @date 2026/4/22
+ */
+@Data
+public class CheckUserExistsRespBO {
+    private Boolean exists;
+}
