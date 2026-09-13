779666b4695fef3630f68f40ee69e6237d57fee6
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:43:05 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconAccount.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="12" cy="8" r="4" />
+    <path d="M5 20c0-4 3.5-7 7-7s7 3 7 7" />
+  </svg>
+</template>
