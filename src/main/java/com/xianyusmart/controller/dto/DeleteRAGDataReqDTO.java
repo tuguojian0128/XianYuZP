ab9cfb7adbfdc0d912fe7ea4b16b989cc5dd56c6
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:27:08 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/DeleteRAGDataReqDTO.java
@@ -0,0 +1,12 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * @date 2026/4/22
+ * @description 删除RAG资料请求
+ */
+@Data
+public class DeleteRAGDataReqDTO {
+    private String documentId;
+}
