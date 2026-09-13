3b05a327a5fc9fc0a7da24af44ac35f635ffde77
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:28:29 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/OrderListRespDTO.java
@@ -0,0 +1,19 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+import java.util.List;
+
+/**
+ * 订单列表响应DTO（第三方调用）
+ */
+@Data
+public class OrderListRespDTO {
+
+    private List<OrderDTO> records;
+
+    private Long total;
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+}
