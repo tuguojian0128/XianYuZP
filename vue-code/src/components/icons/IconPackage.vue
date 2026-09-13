230d75ab803555d6512526adf67a35d992bd8fbe
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:46:27 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconPackage.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M3 9l9-6 9 6v6l-9 6-9-6V9z" />
+    <path d="M3 9l9 6 9-6" />
+    <path d="M12 15v6" />
+  </svg>
+</template>
