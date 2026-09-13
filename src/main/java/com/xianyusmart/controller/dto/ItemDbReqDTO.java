63335cfce77d0d8a374e9fe74b635cab8a5bc3e7
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:27:22 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/controller/dto/ItemDbReqDTO.java
@@ -0,0 +1,14 @@
+package com.xianyusmart.controller.dto;
+
+import lombok.Data;
+
+/**
+ * 从数据库获取商品请求DTO
+ */
+@Data
+public class ItemDbReqDTO {
+    /**
+     * 账号ID
+     */
+    private String cookieId;
+}
