61557dd3193930305003bac62a6114ec66f94202
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:43:19 +0800

    XianYuZP 0.0.1

---
+++ vue-code/src/components/icons/IconChart.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M3 3v18h18" />
+    <path d="M7 16l4-4 4 4 5-8" />
+  </svg>
+</template>
