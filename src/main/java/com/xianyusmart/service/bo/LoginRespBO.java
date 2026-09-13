b2e9c44a9e42f958fa7740195e9dfeabe8974bfd
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:34:37 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/service/bo/LoginRespBO.java
@@ -0,0 +1,13 @@
+package com.xianyusmart.service.bo;
+
+import lombok.Data;
+
+/**
+ * 登录响应BO
+ * @date 2026/4/22
+ */
+@Data
+public class LoginRespBO {
+    private String token;
+    private String username;
+}
