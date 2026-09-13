551f7d89e898109bb8a24a02197b31ea69e0af46
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:31:44 +0800

    XianYuZP 0.0.1

---
+++ src/main/java/com/xianyusmart/mapper/XianyuKamiUsageRecordMapper.java
@@ -0,0 +1,9 @@
+package com.xianyusmart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xianyusmart.entity.XianyuKamiUsageRecord;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface XianyuKamiUsageRecordMapper extends BaseMapper<XianyuKamiUsageRecord> {
+}
