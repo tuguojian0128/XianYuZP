987ba9089451aa144b77c0111437622646a424ca
tuguojian0128 <tuguojian0128@gmail.com>
2026-09-13 16:38:10 +0800

    XianYuZP 0.0.1

---
+++ src/main/resources/static/assets/IconChevronLeft-BGsNF325.js
@@ -0,0 +1,1 @@
+import{_ as o,g as n,b as r,e as t}from"./index-BmZXdVT7.js";const s={},c={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":"1.5","stroke-linecap":"round","stroke-linejoin":"round"};function l(a,e){return n(),r("svg",c,[...e[0]||(e[0]=[t("path",{d:"M15 18l-6-6 6-6"},null,-1)])])}const _=o(s,[["render",l]]);export{_ as I};
